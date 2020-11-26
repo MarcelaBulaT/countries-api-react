5760b6c8bf8f8ea0f8aa252020f5963caaf356e2
Santiago gaviria <santi20alejo@hotmail.com>
2020-11-26 14:37:10 -0500

    proyecto pais

--- my-proyect/src/country.js
+++ my-proyect/src/country.js
@@ -1,47 +1,58 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
+import { useHistory } from "react-router-dom";
 
 const CountryStyled = styled.div`
-width: 264px;
-text-align: left; 
-border-radius: 5px;
-margin: 1em;
-overflow: hidden;
-box-shadow: 0 0 7px 2px rgba(0,0,0,.3);
-img{
+  text-align: left;
+  border-radius: 5px;
+  margin: 1em;
+  overflow: hidden;
+  box-shadow: 0 0 7px 2px rgba(0, 0, 0, 0.3);
+  img {
     width: 100%;
-    height: 160px; 
-    object-fit: cover; 
-}
-.informacion{
+    height: 160px;
+    object-fit: cover;
+  }
+  .informacion {
     padding: 1.5em;
-}
-.h2{
-    margin:0;
+  }
+  .h2 {
+    margin: 0;
     margin-bottom: 1rem;
     font-size: 20px;
     font-weight: 700;
+  }
+  .p {
+    font-size: 0.9em;
+    marigin-bottom: 0.5rem;
+  }
+`;
+
+function Country({ flag, name, population, region, capital }) {
+  const navegar = useHistory();
+  function DetallesClick() {
+    navegar.push("/country/${name}");
+  }
+  return (
+    <CountryStyled onClick={DetallesClick}>
+      <img loading="lazy" src={flag} alt="" />
+      <div className="informacion">
+        <h2>{name}</h2>
+        <p>
+          <strong>Population:</strong>
+          {population}
+        </p>
+        <p>
+          <strong>Region:</strong>
+          {region}
+        </p>
+        <p>
+          <strong>Capital:</strong>
+          {capital}
+        </p>
+      </div>
+    </CountryStyled>
+  );
 }
-.p{
-    font-size: .9em;
-    marigin-bottom: .5rem;
-}
-`
 
-
-function Country({ flag, name, population, region, capital, }) {
-
-    return (
-        <CountryStyled>
-            <img loading="lazy" src={flag} alt="" />
-            <div className="informacion">
-            <h2>{name}</h2>
-            <p><strong>Population:</strong>{population}</p>
-            <p><strong>Region:</strong>{region}</p>
-            <p><strong>Capital:</strong>{capital}</p>
-            </div>
-        </CountryStyled>
-    )
-}
-
-export default Country
+export default Country;
